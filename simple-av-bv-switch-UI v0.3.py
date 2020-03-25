4f24ad460335d6314152c582ae4c9713763f778f
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2020-03-25 15:31:15 +0800

    v0.3 update
    添加v0.3的.py，保留v0.2的.py

---
+++ simple-av-bv-switch-UI v0.3.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+root = Tk()
+root.title("av-bv Switch")
+root.resizable(width=True, height=True)
+
+Label(root, text= "输入：").grid(row=0, column=0)
+Label(root, text= "输出：").grid(row=1, column=0)
+
+getin = StringVar()
+inbox = Entry(root, textvariable=getin, width = 30)
+outbox = Text(root, height = 1, width = 30)
+inbox.grid(row=0, column=1)
+outbox.grid(row=1, column=1)
+
+anno = Label(root, text = "输入时省略“av”和“BV”")
+anno.grid(row =3 ,column =1)
+
+def charcheck(stri):
+    import re
+    return bool(re.search('[a-z]', stri))
+
+def vswitch():
+    from urllib import request
+    import json
+
+    outbox.delete('1.0', END)
+    stri = inbox.get()
+    c = int(charcheck(stri))
+    if c:
+        html = request.urlopen(r'http://api.bilibili.com/x/web-interface/view?bvid='+stri)
+    else:
+        html = request.urlopen(r'http://api.bilibili.com/x/web-interface/view?aid='+stri)
+    getjson = json.loads(html.read())
+    if getjson['code'] == 0:
+        if c:
+            outbox.insert(INSERT, 'av')
+            outbox.insert(END, getjson['data']['aid'])
+        else:
+            outbox.insert(INSERT, getjson['data']['bvid'])
+    else:
+        print("稿件不可见。")
+
+def clear():
+    inbox.delete(0, END)
+    outbox.delete('1.0', END)
+
+btn = Button(root, text = "转换", width = 10, command = vswitch)
+btn.grid(row = 3 ,column = 0, padx=10, pady =5)
+
+delbtn = Button(root, text = "清除", width = 8, command = clear)
+delbtn.grid(row = 0, column = 2, padx=10, pady =5)
+
+root.mainloop()
